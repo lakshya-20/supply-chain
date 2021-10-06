f1b2ec92bec2c1d52df0aa385b3504853a6e52bc
Lakshya Bansal <39180629+lakshya-20@users.noreply.github.com>
2021-10-06 23:27:54 +0530

    Util functions to add and verify various stakeholders (#4)
    * Util functions to add and verify farmers
    
    * Util functions to add stackholders

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,5 @@
-const Migrations = artifacts.require("Migrations");
+const Main = artifacts.require("Main");
 
-module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+module.exports = function (deployer) {
+  deployer.deploy(Main);
 };
