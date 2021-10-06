3c1a6bb0e4ef5397cdcc8740890e233968697997
Lakshya Bansal <bansallakshya94128@gmail.com>
2021-10-06 17:36:51 +0530

    Util functions to add and verify farmers

--- src/Contracts/Main.sol
+++ src/Contracts/Main.sol
@@ -1,12 +1,12 @@
 pragma solidity ^0.5.0;
-
+pragma experimental ABIEncoderV2;
 contract Main{
     struct Farmer{
         address id;
         string name;
         string region;
         bool isVerified;
-        string [] products;        
+        string [] rawProducts;        
     }
     struct Manufacturer{
         address id;
@@ -32,8 +32,35 @@ contract Main{
         Role role;
     }
 
-    mapping(address => Farmer) farmers;
-    mapping(address => Manufacturer) manufacturers;
-    mapping(string => Product) products;
-    mapping(address => Stakeholder) stakeholders;
+    mapping(address => Farmer) public farmers;
+    mapping(address => Manufacturer) public manufacturers;
+    mapping(string => Product) public products;
+    mapping(address => Stakeholder) public stakeholders;
+
+    address payable public admin;
+    constructor() public {
+        admin = msg.sender;        
+    }
+
+    // Modifier to ensure only the admin access a function
+    modifier verifyAdmin( address id) {
+        require(id == admin, "Unauthorized");
+        _;
+    }
+
+    /**
+     * Function to register a farmer on the application
+     */
+    function registerFarmer(
+        string memory name,
+        string memory region,
+        string[] memory rawProducts
+    ) public {
+        farmers[msg.sender] = Farmer(msg.sender,name,region,false,rawProducts);
+    }
+    
+    function verifyFarmer(address id) public verifyAdmin(msg.sender) {
+        farmers[id].isVerified = true;
+    }
+
 }
