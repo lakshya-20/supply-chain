db416e9c37b6b7fee5aa0f52557921366dae05e4
Lakshya Bansal <39180629+lakshya-20@users.noreply.github.com>
2021-10-06 23:27:54 +0530

    Util functions to add and verify various stakeholders (#4)
    * Util functions to add and verify farmers
    
    * Util functions to add stackholders

--- migrations/2_deploy_contracts.js
+++
@@ -1,2 +0,0 @@
-module.exports = async function(deployer, network, accounts) {
-}
--- src/Contracts/Main.sol
+++ src/Contracts/Main.sol
@@ -1,12 +1,12 @@
 pragma solidity ^0.5.0;
-
+pragma experimental ABIEncoderV2;
 contract Main{
     struct Farmer{
         address id;
         string name;
         string region;
         bool isVerified;
-        string [] products;        
+        string [] rawProducts;        
     }
     struct Manufacturer{
         address id;
@@ -24,16 +24,100 @@ contract Main{
         string serialNo;
     }
 
-    enum Role {Distributer, retailer, consumer}
+    // enum Role {Distributer, Retailer, Consumer, Manufacturer}
 
     struct Stakeholder{
         address id;
         string name;
-        Role role;
+        string role;
     }
 
-    mapping(address => Farmer) farmers;
-    mapping(address => Manufacturer) manufacturers;
-    mapping(string => Product) products;
-    mapping(address => Stakeholder) stakeholders;
+    mapping(address => Farmer) public farmers;
+    mapping(address => Manufacturer) public manufacturers;
+    mapping(string => Product) public products;
+    mapping(address => Stakeholder) public stakeholders;
+
+    address payable public admin;
+    constructor() public {
+        admin = msg.sender;        
+    }
+
+    // Modifier to ensure only the admin access a function
+    modifier verifyAdmin( address id) {
+        require(id == admin, "Unauthorized");
+        _;
+    }
+
+    /**
+     * Function to add a new farmer on the application
+     */
+    function registerFarmer(
+        string memory name,
+        string memory region,
+        string[] memory rawProducts
+    ) public {
+        farmers[msg.sender] = Farmer(msg.sender,name,region,false,rawProducts);
+    }
+    /**
+     * Function to verify a farmer
+     */
+    function verifyFarmer(address id) public verifyAdmin(msg.sender) {
+        farmers[id].isVerified = true;
+    }
+    /**
+     * function to find a farmer by address
+     */
+    function findFarmer(address id) public view returns(Farmer memory){
+        return farmers[id];
+    }
+
+    // /**
+    //  * Function to add a new manufacturer on the application
+    //  */
+    // function registerManufacturer(string memory name) public {
+    //     manufacturers[msg.sender] = Manufacturer({
+    //         id: msg.sender,
+    //         name: name,
+    //         isRenewableUsed: false
+    //     });
+    // }
+    /**
+     * Function to set the isRenewableUsed for a manufacturer
+     */
+    function updateManufacturerRenewable(address id) public verifyAdmin(msg.sender){
+        manufacturers[id].isRenewableUsed = true;
+    }
+    /**
+     * Function to add a rawproduct for a manufacturer
+     */
+    function updateManufacturerRawProducts(        
+        string memory rawProductName,
+        address farmerAddress
+    ) public {
+        manufacturers[msg.sender].rawProducts[rawProductName] = farmerAddress;
+    }
+    /**
+     * function to find a manufacturer by address
+     */
+    function findManufacturer(address id) public view returns(string memory,bool){
+        return(
+            manufacturers[id].name,
+            manufacturers[id].isRenewableUsed
+        );
+    }
+
+    function registerStakeHolder(
+        string memory name,
+        string memory role
+    ) public {
+        stakeholders[msg.sender] = Stakeholder(msg.sender,name,role);
+        if(keccak256(abi.encodePacked(role)) == keccak256(abi.encodePacked("Manufacturer"))){
+            manufacturers[msg.sender] = Manufacturer({
+                id: msg.sender,
+                name: name,
+                isRenewableUsed: false
+            });
+        }
+    }
+
 }
