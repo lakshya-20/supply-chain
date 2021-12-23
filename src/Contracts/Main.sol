82e672e3bd8e3a0c84c47d5ae3680f33e67ba4d1
Lakshya Bansal <bansallakshya94128@gmail.com>
2021-12-23 13:19:43 +0530

    Restructured application state layout

--- src/Contracts/Main.sol
+++ src/Contracts/Main.sol
@@ -7,15 +7,24 @@ import './Product.sol';
 
 contract Main{
 
+    address public adminAddress;
     Farmer public farmer;
     Manufacturer public manufacturer;
     StakeHolder public stakeHolder;
     Product public product;
-
+    
     constructor(Farmer _farmer, Manufacturer _manufacturer, StakeHolder _stakeHolder, Product _product) public {
         farmer = _farmer;
         manufacturer = _manufacturer;
         stakeHolder = _stakeHolder;
         product = _product;
+        adminAddress = msg.sender;
     }
+
+    // function getRole(address id) public view returns(string memory){
+    //     if(farmer.getFarmer(id).isValue) return "farmer";
+    //     else if(manufacturer.getManufacturer(id).isValue) return "manufacturer";
+    //     else if(stakeHolder.getStakeHolder(id).isValue) return stakeHolder.getStakeHolder(id).role;
+    //     else return "new_address";
+    // }
 }
