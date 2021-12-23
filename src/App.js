9f091eb8ec5397ebc7d5f593ddd546f26bfefa4f
Lakshya Bansal <bansallakshya94128@gmail.com>
2021-12-23 13:19:43 +0530

    Restructured application state layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import { useEffect, useState } from 'react';
-import Web3 from 'web3';
 import { Card, Button, CardTitle, CardText } from 'reactstrap';
-import MainContract from './Abis/Main.json';
 import './App.css';
+import Web3 from 'web3';
+
+import MainContract from './Abis/Main.json';
+import FarmerContract from './Abis/Farmer.json';
+import ManufacturerContract from './Abis/Manufacturer.json';
+import StakeHolderContract from './Abis/StakeHolder.json';
+import ProductContract from './Abis/Product.json';
+
 import Admin from './Components/AdminScreens/Admin';
 import ConsumerComponent from './Components/Consumer';
 import FooterComponent from './Components/Footer';
@@ -12,22 +18,22 @@ import Register from './Components/Register';
 
 function App() {
   const [mainContract, setMainContract] = useState(undefined);
+  const [farmerContract, setFarmerContract] = useState(undefined);
+  const [manufacturerContract, setManufacturerContract] = useState(undefined);
+  const [stakeHolderContract, setStakeHolderContract] = useState(undefined);
+  const [productContract, setProductContract] = useState(undefined);
+  
   const [adminAddress, setAdminAddress] = useState(undefined);
-  const [account, setAccount] = useState(undefined);
-  const [role, setRole] = useState(undefined);
+  const [currAddress, setCurrAddress] = useState(undefined);
+  const [currAddressRole, setCurrAddressRole] = useState(undefined);
   const [stakeholder, setStakeholder] = useState(undefined);
+
   useEffect(()=>{
     (async () => {
       await loadWeb3();
       await loadBlockchainData();
     })();
   },[])
-
-  useEffect(()=>{
-    (async () => {
-      await loadStackholderData();
-    })();
-  },[mainContract])
 
   const loadWeb3 = async () =>{
     if (window.ethereum) {
@@ -41,36 +47,57 @@ function App() {
       window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
     }
   }
+
   const loadBlockchainData = async () =>{
     const web3 = window.web3;
     const accounts = await web3.eth.getAccounts();
     const account = accounts[0];
-    setAccount(account);
-    const networkId = await web3.eth.net.getId();
-    const mainContractData = MainContract.networks[networkId];
-    if(mainContractData){
-      const main = new web3.eth.Contract(MainContract.abi, mainContractData.address)      
-      setRole(await main.methods.checkIdentity(account).call());
-      setAdminAddress(await main.methods.admin().call());
-      setMainContract(main);
+    setCurrAddress(account);
+    const networkId = await web3.eth.net.getId();   
+    const main = new web3.eth.Contract(MainContract.abi, MainContract.networks[networkId].address);
+    const farmer = new web3.eth.Contract(FarmerContract.abi, FarmerContract.networks[networkId].address); 
+    const manufacturer = new web3.eth.Contract(ManufacturerContract.abi, ManufacturerContract.networks[networkId].address);
+    const stakeHolder = new web3.eth.Contract(StakeHolderContract.abi, StakeHolderContract.networks[networkId].address);
+    const product = new web3.eth.Contract(ProductContract.abi, ProductContract.networks[networkId].address);
+    await Promise.all([
+      setMainContract(main),
+      setFarmerContract(farmer),
+      setManufacturerContract(manufacturer),
+      setStakeHolderContract(stakeHolder),
+      setProductContract(product)
+    ]);
+    setAdminAddress(await main.methods.adminAddress().call());
+    const farmerData = await farmer.methods.getFarmer(account).call();
+    const manufacturerData = await manufacturer.methods.getManufacturer(account).call();
+    const stakeHolderData = await stakeHolder.methods.getStakeHolder(account).call();
+    if(account == adminAddress){
+      setCurrAddressRole("Admin");
     }
-  }
-
-  const loadStackholderData = async () => {
-    if(role==="Farmer") setStakeholder(await mainContract.methods.findFarmer(account).call());
-    else if(["Distributer", "Retailer", "Consumer", "Manufacturer"].includes(role)) setStakeholder(await mainContract.methods.findStakeholder(account).call());
+    else if(farmerData.isValue) {
+      setCurrAddressRole("Farmer");
+      setStakeholder(farmerData);
+    }
+    else if(manufacturerData.isValue) {
+      setCurrAddressRole("manufacturer");
+      setStakeholder(manufacturerData);
+    }
+    else if(stakeHolderData.isValue) {
+      setCurrAddressRole(stakeHolderData.role);
+      setStakeholder(stakeHolderData);
+    }
+    else setCurrAddressRole("new_address");
   }
 
   return (
     <>
-    <NavbarComponent mainContract={mainContract} account={account} />
+    <NavbarComponent account={currAddress} />
     <div className="App container" style={{marginBottom: "100px", minHeight:"500px"}}>
       {stakeholder?
         <div className="d-flex justify-content-center text-center">
           <div className="col-10 col-sm-8">
             <Card inverse style={{ backgroundColor: '#333', borderColor: '#333' }}>
               <CardTitle tag="h5">{stakeholder.name}</CardTitle>
-              {role=="Farmer"?
+              {currAddressRole=="Farmer"?
                 <>
                 <CardText>
                   Role: Farmer
@@ -88,19 +115,17 @@ function App() {
         </div>
       :
         ""
-      }      
-      {/* {stakeholder? `Stakeholder Data: ${JSON.stringify(stakeholder)}` : ""} */}
-      {role==="NewAddress"? <Register mainContract={mainContract} account={account}/> : ""}
+      }            
+      {/* {role==="NewAddress"? <Register mainContract={mainContract} account={account}/> : ""}
       {role==="Admin"? <Admin mainContract={mainContract} account={account}/> : ""}
       {role==="Manufacturer"? <Manufacturer mainContract={mainContract} account={account}/> : ""}
       {role==="Distributer"? <ConsumerComponent mainContract={mainContract} account={account} role={role}/> : ""}
-      {role==="Consumer"? <ConsumerComponent mainContract={mainContract} account={account} role={role}/> : ""}
+      {role==="Consumer"? <ConsumerComponent mainContract={mainContract} account={account} role={role}/> : ""} */}
     </div>
     <FooterComponent 
-      mainContract={mainContract} 
-      account={account}
+      account={currAddress}
       adminAddress = {adminAddress}
-      role={role}
+      role={currAddressRole}
     />
     </>
   );
