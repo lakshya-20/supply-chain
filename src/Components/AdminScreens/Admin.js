1f47240aee34ad28538d35246fcf1bf70cba61d9
Lakshya Bansal <bansallakshya94128@gmail.com>
2021-12-25 01:26:09 +0530

    Admin verification for farmer and manufacturer

--- src/Components/AdminScreens/Admin.js
+++ src/Components/AdminScreens/Admin.js
@@ -1,16 +1,16 @@
 import { Row, Col} from 'reactstrap';
 import VerifyFarmer from "./VerifyFarmer";
 import VerifyManufacturer from "./VerifyManufacturer";
-const Admin = ({mainContract,account}) => {
+const Admin = ({farmerContract, manufacturerContract, account}) => {
     return (
         <div className="text-center">
             <h3>Admin Panel</h3>
             <Row>
                 <Col sm="6" className="p-1">
-                    <VerifyFarmer mainContract={mainContract} account={account}/>
+                    <VerifyFarmer farmerContract={farmerContract} account={account}/>
                 </Col>
                 <Col sm="6" className="p-1">
-                    <VerifyManufacturer mainContract={mainContract} account={account}/>
+                    <VerifyManufacturer manufacturerContract={manufacturerContract} account={account}/>
                 </Col>
             </Row>
         </div>
--- src/Components/AdminScreens/VerifyManufacturer.js
+++ src/Components/AdminScreens/VerifyManufacturer.js
@@ -1,41 +1,44 @@
 import React, { useState, useEffect } from 'react';
 import { Card, CardSubtitle, CardTitle, CardText} from 'reactstrap';
 import Manufacturer from '../Manufacturer';
-const VerifyManufacturer = ({mainContract,account}) => {
+const VerifyManufacturer = ({manufacturerContract, account}) => {
     const [manufacturerDetailArray, setManufacturerDetailArray] = useState([]);
+    
+    useEffect(()=>{
+        if(manufacturerContract){
+            (async()=>{
+                const addressArray =  await manufacturerContract.methods.getManufacturersList().call();
+                var temp=[];
+                for(var i=0;i<addressArray.length;i++){
+                    temp[i]=await manufacturerContract.methods.getManufacturer(addressArray[i]).call();
+                }                
+                setManufacturerDetailArray(temp);
+            })();
+        }
+    },[manufacturerContract])
+
     const verifyManufacturer = async (manufacturerAddress) => {
         if(manufacturerAddress==null) {
             alert("Please provide a address");
             return;
         }
-        await mainContract.methods.updateManufacturerRenewable(manufacturerAddress).send({from: account});
+        await manufacturerContract.methods.verifyManufacturer(manufacturerAddress).send({from: account});
         window.location.reload(false);
     }
-    useEffect(()=>{
-        if(mainContract){
-            (async()=>{
-                const addressArray =  await mainContract.methods.getManufacturersArray().call();
-                var temp=[];
-                for(var i=0;i<addressArray.length;i++){
-                    temp[i]=await mainContract.methods.findManufacturer(addressArray[i]).call();
-                }                
-                setManufacturerDetailArray(temp);
-            })();
-        }
-    },[mainContract])
+    
     const renderManufacturerCard=(manufacturer)=>{
-        //console.log(manufacturer[0])
         return (
             <Card className="m-1">                
-                <CardTitle>{manufacturer[0]}</CardTitle>   
-                <CardSubtitle className="text-secondary">{manufacturer[2]}</CardSubtitle>             
+                <CardTitle>{manufacturer.name}</CardTitle>   
+                <CardSubtitle className="text-secondary">{manufacturer.id}</CardSubtitle>             
+                <CardSubtitle>Raw Products: {JSON.stringify(manufacturer.rawProducts)}</CardSubtitle>           
                 <div className="">
-                    {manufacturer[1]?
+                    {manufacturer.isRenewableUsed?
                         <span className="text-success">
                             Renewable Resources
                         </span>
                     :
-                        <span style={{cursor: "pointer"}} onClick={()=>verifyManufacturer(manufacturer[2])}>
+                        <span style={{cursor: "pointer"}} onClick={()=>verifyManufacturer(manufacturer.id)}>
                             Verify
                         </span>
                     }
