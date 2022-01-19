892628f27df8909d21af43c79c2af0ff1c7a4efc
Lakshya Bansal <bansallakshya94128@gmail.com>
2022-01-20 00:04:22 +0530

    Access contracts in admin components using context api

--- src/Components/Screens/AdminScreens/Admin.js
+++ src/Components/Screens/AdminScreens/Admin.js
@@ -1,16 +1,16 @@
 import { Row, Col} from 'reactstrap';
 import VerifyFarmer from "./VerifyFarmer";
 import VerifyManufacturer from "./VerifyManufacturer";
-const Admin = ({farmerContract, manufacturerContract, account}) => {
+const Admin = () => {
     return (
         <div className="text-center">
             <h3>Admin Panel</h3>
             <Row>
                 <Col sm="6" className="p-1">
-                    <VerifyFarmer farmerContract={farmerContract} account={account}/>
+                    <VerifyFarmer/>
                 </Col>
                 <Col sm="6" className="p-1">
-                    <VerifyManufacturer manufacturerContract={manufacturerContract} account={account}/>
+                    <VerifyManufacturer/>
                 </Col>
             </Row>
         </div>
