a7c47a9d00842991e3975f009b7451d77c9e1665
Lakshya Bansal <bansallakshya94128@gmail.com>
2022-04-25 23:17:41 +0530

    Stakeholder contract misc

--- src/Contracts/Admin.sol
+++ src/Contracts/Admin.sol
@@ -1,8 +1,9 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 contract Admin{
   address public admin;
   
-  constructor(address _admin) public{
+  constructor(address _admin){
     admin = _admin;
   }
   
