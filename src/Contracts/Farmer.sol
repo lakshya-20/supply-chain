f55ef05e9cdd3075cfae343ca91cf3991148b26e
Lakshya Bansal <bansallakshya94128@gmail.com>
2022-04-26 11:31:13 +0530

    Manufacturer Contract Misc

--- src/Contracts/Farmer.sol
+++ src/Contracts/Farmer.sol
@@ -2,17 +2,15 @@
 pragma solidity ^0.8.0;
 import './Stakeholder.sol';
 contract Farmer is Stakeholder {
-  mapping(address => bool) public _farmerVerified;
   mapping(address => string[]) public _farmerRawProducts;
   mapping(string => address[]) public _rawProductFarmers;
 
   constructor() Stakeholder() {}
 
-  function register(string memory _name, string memory _location) public override returns (bool) {
+  function registerFarmer(string memory _name, string memory _location, string[] memory _rawProducts) public returns (bool) {
     require (_stakeholders[msg.sender].id ==  address(0), "Farmer::registerFarmer: Farmer already registered");
-    _stakeholders[msg.sender] = stakeholder(msg.sender, _name, _location, "farmer");
-    _farmerVerified[msg.sender] = false;
-    _farmerRawProducts[msg.sender] = new string[](0);
+    _stakeholders[msg.sender] = stakeholder(msg.sender, _name, _location, "farmer", false);
+    _farmerRawProducts[msg.sender] = _rawProducts;
     return true;
   }
 
@@ -29,20 +27,13 @@ contract Farmer is Stakeholder {
     return true;
   }
 
-  function verify(address farmer) public onlyAdmin returns (bool){
-    _farmerVerified[farmer] = true;
-    return true;
-  }
-
   function getFarmer(address _id) public view onlyStakeholder(_id) returns(
     stakeholder memory,
-    bool farmerVerified,
     string[] memory farmerRawProducts
   ){
     stakeholder memory stakeholder = super.get(_id);
-    farmerVerified = _farmerVerified[_id];
     farmerRawProducts = _farmerRawProducts[_id];
-    return (stakeholder, farmerVerified, farmerRawProducts);
+    return (stakeholder, farmerRawProducts);
   }
 
   function getRawProductFarmers(string memory _rawProduct) public view returns (address[] memory) {
