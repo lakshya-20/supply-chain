f71020a9325a13f11389eeb4107de56b7fc123e1
Lakshya Bansal <bansallakshya94128@gmail.com>
2022-04-29 22:59:30 +0530

    Project structure

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
-import './App.css';
-import {BrowserRouter} from 'react-router-dom'
-import FooterComponent from './Components/Footer';
-import NavbarComponent from './Components/Navbar';
-import Routing from './Routing';
-import AuthContextProvider from './Context/Contexts/AuthContext';
-import ContractContextProvider from './Context/Contexts/ContractContext';
-import JumbotronComponent from './Components/JombotronComponent';
-
+import './Assests/Styles/app.css';
+import Main from './Layouts/Main';
 
 function App() {
   return (
     <div className='App'>
-    <AuthContextProvider>
-    <ContractContextProvider>
-      <NavbarComponent/>
-      <JumbotronComponent/>
-      <BrowserRouter>        
-          <div className='container'>
-            <Routing/>
-          </div>        
-      </BrowserRouter>
-      {/* <FooterComponent/> */}
-      </ContractContextProvider>
-    </AuthContextProvider>
+      <Main/>
     </div>
   );
 }
-
 export default App;
