faebc747f0e6523e5e452d834d285ea341ca27e2
Lakshya Bansal <bansallakshya94128@gmail.com>
2022-04-30 19:54:10 +0530

    Smart Contract context

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,4 +1,9 @@
+import { useContext } from "react";
+import { ContractContext } from "../Services/Contexts/ContractContext";
+
 const Dashboard = () => {
+  const { contractState } = useContext(ContractContext);
+  console.log( contractState );
   return (
     <div>
       Dashboard
