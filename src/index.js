bcacfe88cafae180322130b51454eb01077879a8
Lakshya Bansal <bansallakshya94128@gmail.com>
2022-05-01 18:22:56 +0530

    Dashboard middle wrapper

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.css';
 import 'react-toastify/dist/ReactToastify.css';
+import "react-multi-carousel/lib/styles.css";
 import { ToastContainer} from 'react-toastify';
 ReactDOM.render(
   <React.StrictMode>
