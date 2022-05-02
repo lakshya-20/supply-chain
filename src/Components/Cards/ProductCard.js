d96acd277786f542dcc6a639cde2fe9d93591177
Lakshya Bansal <bansallakshya94128@gmail.com>
2022-05-02 19:25:52 +0530

    Product list page

---
+++ src/Components/Cards/ProductCard.js
@@ -0,0 +1,52 @@
+import product_default from '../../Assests/Images/product_default.jpg';
+const ProductCard = ({product}) => {
+  console.log(product)
+  return (
+    <div className="col-12 col-lg-6 my-1">
+      <div className="row d-flex justify-content-around align-items-center">
+        <div className="col-12 col-md-4">
+          <img 
+            src={product_default}
+            width="100%"
+          />
+        </div>
+        <div className="col-12 col-md-8">
+          <span className="card-key">Id: </span>
+          <span className="card-value">{product.item["id"]}</span>
+          <br/>
+          <span className="card-key">Name: </span>
+          <span className="card-value">{product.item["title"]}</span>
+          <br/>
+          <span className="card-key">Manufacturer: </span>
+          <span className="card-value">{product.manufacturer["name"]}</span>
+          <br/>
+          <span className="card-key">Rating: </span>
+          <span className="card-value">{product.item["rating"]/10}</span>
+          <br/>
+          <span className="d-flex justify-content-around">
+            {product.manufacturer.isVerified?
+              <span className="">
+                <span className="badge bg-success">Verified</span>
+              </span>
+            :
+              <span className="">
+                <span className="badge bg-warning">Not Verified</span>
+              </span>
+            }
+            {product.manufacturer.isRenewableUsed?
+              <span className="">
+                <span className="badge bg-success">Eco Friendly</span>
+              </span>
+            :
+              <span className="">
+                <span className="badge bg-warning">Non Eco Friendly</span>
+              </span>
+            }
+          </span>
+        </div>
+      </div>
+      <hr/>
+    </div>
+  )
+}
+export default ProductCard;
